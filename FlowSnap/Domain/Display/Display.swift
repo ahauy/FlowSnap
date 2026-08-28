6bd569e3d4c06616de5dacc9272299d141ea18c1
ahauy <vutuanhau.haui@gmail.com>
2026-08-28 08:03:10 +0700

    feat(domain): add isPrimary indicator to Display model

--- FlowSnap/Domain/Display/Display.swift
+++ FlowSnap/Domain/Display/Display.swift
@@ -16,15 +16,20 @@ public struct Display: Identifiable, Hashable, Sendable {
     /// Retina scale factor (1.0, 2.0, etc.).
     public let scaleFactor: CGFloat
 
+    /// Indicates whether this display is the macOS Primary Display (origin at (0,0) in AppKit).
+    public let isPrimary: Bool
+
     public init(
         id: CGDirectDisplayID,
         frame: CGRect,
         visibleFrame: CGRect,
-        scaleFactor: CGFloat
+        scaleFactor: CGFloat,
+        isPrimary: Bool? = nil
     ) {
         self.id = id
         self.frame = frame
         self.visibleFrame = visibleFrame
         self.scaleFactor = scaleFactor
+        self.isPrimary = isPrimary ?? (frame.origin == .zero)
     }
 }
